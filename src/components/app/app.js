742ff8ffd381c980b15fa51184f84d5a6a9e7824
Maxim Chayka <119533893+jalisco2213@users.noreply.github.com>
2023-04-26 13:39:38 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,9 +7,9 @@ import EmployeesAddForm from '../employeers-add-form/employeers-add-form';
 
 function App() {
     const data = [
-        { name: 'Ivan K.', salary: 700, increase: false },
-        { name: 'Elizabeth F.', salary: 1210, increase: true },
-        { name: 'German Q.', salary: 960, increase: false },
+        { name: 'Ivan K.', salary: 700, increase: false, id: 1 },
+        { name: 'Elizabeth F.', salary: 1210, increase: true, id: 2 },
+        { name: 'German Q.', salary: 960, increase: false, id: 3 },
     ];
 
     return (
