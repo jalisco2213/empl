4a07854c766e93846785ab6bf8524853c3c57e84
Maxim Chayka <119533893+jalisco2213@users.noreply.github.com>
2023-04-26 20:51:30 +0300

    add find empl

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,7 +14,8 @@ class App extends Component {
                 { name: 'Ivan V.', salary: 1100, increase: false, rise: false, id: 1 },
                 { name: 'Alex D.', salary: 780, increase: false, rise: false, id: 2 },
                 { name: 'Elizabeth M.', salary: 960, increase: false, rise: false, id: 3 }
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -54,9 +55,25 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render() {
+        const { data, term } = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
 
         return (
             <div className="app">
@@ -65,14 +82,14 @@ class App extends Component {
                     increased={increased} />
 
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter />
                 </div>
 
                 <EmployeersList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
-                    onToggleProp={this.onToggleProp}/>
+                    onToggleProp={this.onToggleProp} />
                 <EmployeesAddForm onAdd={this.addItem} />
             </div>
         );
