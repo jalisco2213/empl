5081297ccd4fa1cec7b3bc8107c33741c7cefdd3
Maxim Chayka <119533893+jalisco2213@users.noreply.github.com>
2023-04-26 15:26:10 +0300

    Update employeers-list-item.js

--- src/components/employeers-list-item/employeers-list-item.js
+++ src/components/employeers-list-item/employeers-list-item.js
@@ -1,11 +1,12 @@
 import { Component } from 'react';
 import './employeers-list-item.css';
 
-class EmployeersListItem extends Component {
+class EmployeesListItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
+            increase: false,
+            rise: false
         }
     }
 
@@ -15,23 +16,31 @@ class EmployeersListItem extends Component {
         }))
     }
 
-    render() {
-        let { name, salary } = this.props;
-        let { increase } = this.state;
+    onRise = () => {
+        this.setState(({ rise }) => ({
+            rise: !rise
+        }))
+    }
 
-        let classNames = 'list-group-item d-flex justify-content-between';
+    render() {
+        const { name, salary } = this.props;
+        const { increase, rise } = this.state;
+
+        let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
-            classNames += ' increase';
+            classNames += ' increase like';
+        }
+        if (rise) {
             classNames += ' like';
         }
 
         return (
             <li className={classNames}>
-                <span className="list-group-item-label">{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={'$' + salary} />
+                <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
-                        className="btn-cookie btn-sm"
+                        className="btn-cookie btn-sm "
                         onClick={this.onIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
@@ -47,4 +56,4 @@ class EmployeersListItem extends Component {
     }
 }
 
-export default EmployeersListItem;
+export default EmployeesListItem;
