670f06f6099134090f7d444fa2e0f4113cb3c038
Maxim Chayka <119533893+jalisco2213@users.noreply.github.com>
2023-04-26 12:41:37 +0300

    practice with props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,12 @@ import EmployeersList from '../employeers-list/employeers-list';
 import EmployeesAddForm from '../employeers-add-form/employeers-add-form';
 
 function App() {
+    const data = [
+        { name: 'Ivan K.', salary: 700, increase: false },
+        { name: 'Elizabeth F.', salary: 1210, increase: true },
+        { name: 'German Q.', salary: 960, increase: false },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -15,7 +21,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeersList />
+            <EmployeersList data={data} />
 
             <EmployeesAddForm />
         </div>
